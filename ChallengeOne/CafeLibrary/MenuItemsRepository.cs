93161be14898434cd8b394e80753c43fa5e30485
Alan Murugan <alanmurugan@gmail.com>
2022-01-20 10:06:00 -0500

    Finished successful testing of needed items and repo functions

--- ChallengeOne/CafeLibrary/MenuItemsRepository.cs
+++ ChallengeOne/CafeLibrary/MenuItemsRepository.cs
@@ -7,23 +7,33 @@ namespace CafeLibrary
 {
     public class MenuItemsRepository
     {
-        //Empty repo for Menu Items
+        
+        //Empty repo for Menu Items 
         private readonly List<MenuItem> _repo = new List<MenuItem>();
+        //CRUD
+        //CREATE
+        //Add Menu Items
+        public bool AddMenuItem(MenuItem newItem)
+        {
+            int initialCount = _repo.Count;
+            _repo.Add(newItem);
 
+            return _repo.Count > initialCount;
+        }
+        //READ
         //See All Menu Items
         public List<MenuItem> GetContents()
         {
             return _repo;
         }
-        //Add Menu Items
-        public bool AddMenuItems(MenuItem item)
-        {
-            int initialCount = _repo.Count;
-            _repo.Add(item);
+        //UPDATE (not needed for assignment)
+        //Optional: Update Menu Item
 
-            return _repo.Count > initialCount;
-        }
-
+        //DELETE
         //Delete Menu Items
+        public bool DeleteMenuItem(MenuItem existingItem)
+        {
+            return _repo.Remove(existingItem);
+        }
     }
 }
--- ChallengeOne/CafeTests/MenuItemsRepositoryTests.cs
+++ ChallengeOne/CafeTests/MenuItemsRepositoryTests.cs
@@ -2,11 +2,67 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CafeLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-namespace CafeTests
+namespace MenuItemsTests
 {
+
+    [TestClass]
     public class MenuItemsRepositoryTests
     {
-        
+        private MenuItem _item;
+        private MenuItemsRepository _repo;
+        [TestInitialize]
+
+        //Add menu items
+        [TestMethod]
+        public void AddMenuItem_ShouldReturnTrue()
+        {
+            //Arrange
+            _item = new MenuItem();
+            _repo = new MenuItemsRepository();
+            //Arrange
+            bool result = _repo.AddMenuItem(_item);
+            //Assert
+            Assert.IsTrue(result);
+        }
+
+        //Get all menu items
+        [TestMethod]
+        public void GetAllMenuItems_ShouldReturnCorrectCount()
+        {
+            //Arrange
+            _item = new MenuItem();
+            MenuItem itemTwo = new MenuItem();
+            MenuItem itemThree = new MenuItem();
+            _repo = new MenuItemsRepository();
+            _repo.AddMenuItem(_item);
+            _repo.AddMenuItem(itemTwo);
+            _repo.AddMenuItem(itemThree);
+            //Act
+            List<MenuItem> result = _repo.GetContents();
+            //Assert
+            Assert.AreEqual(3, result.Count);
+        }
+
+        [TestMethod]
+        public void DeleteMenuItem_ShouldReturnFalse()
+        {
+            //Arrange
+            _item = new MenuItem();
+            MenuItem itemTwo = new MenuItem();
+            MenuItem itemThree = new MenuItem();
+            _repo = new MenuItemsRepository();
+            _repo.AddMenuItem(_item);
+            _repo.AddMenuItem(itemTwo);
+            _repo.AddMenuItem(itemThree);
+            //Act
+            _repo.DeleteMenuItem(_item);
+            List<MenuItem> result = _repo.GetContents();
+            //Assert
+            Assert.IsFalse(result.Contains(_item));
+        }
+
     }
 }
