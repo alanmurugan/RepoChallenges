741f4592288f047d23e6f52bac5bc4882af9373f
Alan Murugan <alanmurugan@gmail.com>
2022-01-23 20:44:13 -0500

    Redid scaffolding, object test successful

---
+++ ChallengeThree/BadgeLibrary/Badge.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BadgeLibrary
+{
+    public class Badge
+    {
+        public Badge() { }
+        public Badge(int badgeID, List<string> doorAccess, string badgeName)
+        {
+            BadgeID = badgeID;
+            DoorAccess = doorAccess;
+            BadgeName = badgeName;
+        }
+        public int BadgeID { get; set; }
+        public List<string> DoorAccess { get; set; }
+        public string BadgeName { get; set; }
+
+    }
+}
---
+++ ChallengeThree/BadgeTests/BadgeeObjectTests.cs
@@ -0,0 +1,18 @@
+using System.Collections.Generic;
+using BadgeLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BadgeTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestBadgeConstructor_ShouldSetCorrectValues()
+        {
+            Badge testBadge = new Badge (1234, new List<string> {"A1", "A2", "B1", "B4"}, "Alyssa Cady");
+            Assert.AreEqual(1234, testBadge.BadgeID);
+            CollectionAssert.AreEqual(new List<string> {"A1", "A2", "B1", "B4"}, testBadge.DoorAccess);
+        }
+    }
+}
